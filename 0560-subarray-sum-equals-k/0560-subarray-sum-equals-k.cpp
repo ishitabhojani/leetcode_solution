7a663a5375e50a85aa2bf19a26a20511ec7060dd
Ishita Bhojani <119088819+ishitabhojani@users.noreply.github.com>
2025-02-07 22:13:46 +0530

    Time: 28 ms (99.06%), Space: 45.7 MB (43.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
+        // int n = arr.size();
+
+        // int ans = 0;
+
+        // for(int i = 0; i<n; i++){
+        //     int sum = arr[i];
+        //     if(sum == k){
+        //         ans++;
+        //     }
+        //     for(int j = i+1;j<n;j++){
+        //         sum += arr[j];
+        //         if(sum == k){
+        //             ans++;
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
         int n = arr.size();
+        int prefix[n];
+        prefix[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            prefix[i] = arr[i]+prefix[i-1];
+        }
+
+        unordered_map<int, int> mp;
 
         int ans = 0;
-
-        for(int i = 0; i<n; i++){
-            int sum = arr[i];
-            if(sum == k){
+        for(int i = 0; i<n;i++){
+            if(prefix[i] == k){
                 ans++;
             }
-            for(int j = i+1;j<n;j++){
-                sum += arr[j];
-                if(sum == k){
-                    ans++;
-                }
+
+            if(mp.find(prefix[i] - k) != mp.end()){
+                ans += mp[prefix[i] - k];
             }
+            mp[prefix[i]]++;
         }
 
         return ans;
